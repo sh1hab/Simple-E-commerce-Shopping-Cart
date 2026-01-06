dc018b9165970fef221ebea6b581868539d4b7dd
shihab <shihab.gsc10@gmail.com>
2026-01-06 09:54:48 +0600

    project initialised

---
+++ app/Models/Product.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'stock_quantity',
+        'image',
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+    ];
+
+    public function cartItems(): HasMany
+    {
+        return $this->hasMany(CartItem::class);
+    }
+
+    public function orderItems(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function isLowStock(): bool
+    {
+        return $this->stock_quantity <= 10;
+    }
+}
