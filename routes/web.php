bc2649a75050619eb3877e5b19ff2a6e3c738e15
shihab <shihab.gsc10@gmail.com>
2026-01-07 23:36:19 +0600

    add routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,22 +19,31 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect()->route('products.index');
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/products', [ProductController::class, 'index'])
+        ->name('products.index');
+    
+    Route::get('/cart', [CartController::class, 'index'])
+        ->name('cart.index');
+    Route::post('/cart', [CartController::class, 'store'])
+        ->name('cart.store');
+    Route::patch('/cart/{cartItem}', [CartController::class, 'update'])
+        ->name('cart.update');
+    Route::delete('/cart/{cartItem}', [CartController::class, 'destroy'])
+        ->name('cart.destroy');
+    Route::post('/checkout', [CartController::class, 'checkout'])
+        ->name('cart.checkout');
+    
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
 });
+
 
 require __DIR__.'/auth.php';
