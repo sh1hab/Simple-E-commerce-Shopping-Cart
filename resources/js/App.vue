e1810ebe2edfa6540b6cbe93d3d576fd1bfec5b9
shihab <shihab.gsc10@gmail.com>
2026-01-07 23:39:14 +0600

    add app.vue

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterView } from 'vue-router';
+import { useAuthStore } from './stores/auth';
+import { onMounted } from 'vue';
+
+const authStore = useAuthStore();
+
+onMounted(() => {
+    authStore.checkAuth();
+});
+</script>
+
+<template>
+    <div id="app" class="min-h-screen bg-gray-100">
+        <RouterView />
+    </div>
+</template>
