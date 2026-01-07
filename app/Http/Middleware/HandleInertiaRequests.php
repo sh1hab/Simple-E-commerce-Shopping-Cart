1479d04be05883b7a2b9ef188d293a91f9062f72
shihab <shihab.gsc10@gmail.com>
2026-01-07 23:39:56 +0600

    update HandleInertiaRequests

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -34,6 +34,11 @@ class HandleInertiaRequests extends Middleware
             'auth' => [
                 'user' => $request->user(),
             ],
+            'flash' => [
+                'success' => fn () => $request->session()->get('success'),
+                'error' => fn () => $request->session()->get('error'),
+            ],
+            'cartCount' => $request->user() ? $request->user()->cartItems()->sum('quantity') : 0,
         ];
     }
 }
